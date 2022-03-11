3baeaea3bec03686031c2db409b21e99b298a976
unknown <alix85aliw85@gmail.com>
2022-03-11 14:33:30 +0330

    soal3

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int number;
+double sum=0;
+int counter=-1;
+
+do
+{
+
+System.Console.WriteLine("nomre bede");
+number=Convert.ToInt32(Console.ReadLine());
+
+sum=sum+number;
+counter++;
+
+    
+} while (number!=0);
+
+
+System.Console.WriteLine(sum/counter);
